0a5e077d40f5af4ad375d14b8cea73da639ceb24
KingPr0f <strange98752@gmail.com>
2024-07-06 01:40:33 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import HeroSection from './components/HeroSection.jsx'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+    <HeroSection />
+    </div>
+    
+    </>
+  )
+}
+
+export default App
